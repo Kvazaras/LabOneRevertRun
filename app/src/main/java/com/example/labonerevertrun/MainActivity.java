f7076ee8954ea937636ec5ab6ad11d8486853004
kvazaras <martas.ambraziunas@gmail.com>
2021-09-20 14:38:10 +0100

    Added code to change tvMain text

--- app/src/main/java/com/example/labonerevertrun/MainActivity.java
+++ app/src/main/java/com/example/labonerevertrun/MainActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.tvMain = findViewById(R.id.tvMain);
     }
 
     public void onBtnClick(View view) {
+        this.tvMain.setText("Text was changed!!!");
     }
 }
